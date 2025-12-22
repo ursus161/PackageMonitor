9ef8cea853011a7fd8abd74e3568542d08ea589e
stefan <stefanghetu9@gmail.com>
2025-12-22 13:27:20 +0000

    Crearea fisierului CSV
    Am creat un fisier CSV pentru a organiza datele in mod meticulos si pentru a face mai usoare parsarea lor ulterioara, de asemenea am pus pe primul rand tag-ul coloanei respective

--- backend/monitor.sh
+++ backend/monitor.sh
@@ -2,8 +2,11 @@
 
 #tema itbi branch backend 
 
-grep -E "status installed|status removed" /var/log/dpkg.log | \
+csv_file="../data/packages.csv"
+
+echo "timestamp, status, packet, version" > "$csv_file"
+
+grep -E "status installed|status removed" /var/log/dpkg.log | awk '{print $1" "$2","$4","$5","$6}' | sed 's/:amd64//g; s/:i386//g; s/:all//g' >> "$csv_file"
 #am luat doar liniie cu pachetele instalate sau sterse pt ca cel mai probabil la urmatoarea rulare se vor adauga si cele actualizate
-awk '{print $1" "$2", "$4", "$5", "$6}' | \ 
-sed 's/:amd64//g; s/:i386//g; s/:all//g'
-#elimin arhitectura (amd64, intel, etc)
+#elimin arhitectura (amd64, intel, etc) cu sed
+#aici in mod evident le dau append in fisierul CSV
