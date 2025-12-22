50468e13a392e8a2a44a78eceecbd4057d447099
stefan <stefanghetu9@gmail.com>
2025-12-22 15:59:55 +0000

    Debug stdout
    append-ul se intampla in fisierul principal in loc de main

--- backend/monitor.sh
+++ backend/monitor.sh
@@ -12,7 +12,7 @@ if [ ! -f "$csv_file" ]; then
 fi 
 
 
-grep -E "status installed|status removed" /var/log/dpkg.log | awk '{print $1" "$2","$4","$5","$6}' | sed 's/:amd64//g; s/:i386//g; s/:all//g' >> "$csv_file"
+grep -E "status installed|status removed" /var/log/dpkg.log | awk '{print $1" "$2","$4","$5","$6}' | sed 's/:amd64//g; s/:i386//g; s/:all//g' >> "$fisier_de_lucru"
 #am luat doar liniie cu pachetele instalate sau sterse pt ca cel mai probabil la urmatoarea rulare se vor adauga si cele actualizate
 #elimin arhitectura (amd64, intel, etc) cu sed
 
